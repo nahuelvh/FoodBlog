ebc11b8a31f25581f5d57ebc4d18566b97dc0046
Nahuel Van Houtte <nahuel.vh@hotmail.com>
2022-04-29 21:36:48 -0300

    Tercera versión del FoodBlog

--- AppFoodUsers/models.py
+++ AppFoodUsers/models.py
@@ -10,3 +10,6 @@ class Usuario(models.Model):
     descripcion = models.CharField(max_length=120, null=True, blank=True)
     url = models.URLField(null=True, blank=True)
     avatar = models.ImageField(upload_to='avatares', null=True, blank=True)
+
+def __str__(self):
+        return f"{self.user}"
